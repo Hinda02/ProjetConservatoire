7219130887ce5e7b1957163e3266fbdfa35ba367
lisa <lisa@MBP-de-lisa.lan>
2023-03-28 01:11:31 +0200

    login html

---
+++ resources/views/login.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="index.css">
+  <title>HASH TECHIE OFFICIAL</title>
+</head>
+<body>
+    <section>
+        <div class="form-box">
+            <div class="form-value">
+                <form action="">
+
+                    <div class="inputbox">
+
+                        <input type="text" required>
+                        <label for="">Identifiant</label>
+                    </div>
+                    <div class="inputbox">
+
+                        <input type="text" required>
+                        <label for="">Mot de passe</label>
+                    </div>
+
+
+
+                    <button>Connexion</button>
+                    <div class="register">
+                        <p> <a href="#"></a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js%22%3E</script>
+    <script nomodule src=>
+</body>
+</html>
