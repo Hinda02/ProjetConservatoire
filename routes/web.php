5e9b0c3ee4fbd6ad57920c59a80f3fd0f56e2bcb
lisa <lisa@MBP-de-lisa.lan>
2023-03-28 01:21:28 +0200

    affichage login

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view("welcome");
-});
+//Route::get('/', function () {
+//    return view("welcome");
+//});
 
 Route::get('/', function () {
-    return "j'ai enfin reussi ;)" ;
+    return views("login") ;
 });
