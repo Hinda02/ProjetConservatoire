2beacd76c21d9c747cc427679da25b43ac0b0dfd
lisa <lisa@MBP-de-lisa.lan>
2023-04-02 18:35:42 +0200

    ajout class prof

---
+++ Model/Eleve.php
@@ -0,0 +1,42 @@
+<?php
+
+class Eleve
+{
+
+    private $id;
+    private $bourse;
+    
+    
+
+	/**
+	 * @return mixed
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @param mixed $id 
+	 * @return self
+	 */
+	public function setId($id): self {
+		$this->id = $id;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getBourse() {
+		return $this->bourse;
+	}
+
+	/**
+	 * @param mixed $bourse 
+	 * @return self
+	 */
+	public function setBourse($bourse): self {
+		$this->bourse = $bourse;
+		return $this;
+	}
+}
