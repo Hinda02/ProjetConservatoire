e676489b6f63b40d67efe2a0841ab0fbb8b4276f
lisa <lisa@MBP-de-lisa.lan>
2023-04-02 22:03:57 +0200

    class heure

---
+++ Model/Heure.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class Heure {
+
+    private $tranche; 
+
+	/**
+	 * @return mixed
+	 */
+	public function getTranche() {
+		return $this->tranche;
+	}
+
+	/**
+	 * @param mixed $tranche 
+	 * @return self
+	 */
+	public function setTranche($tranche): self {
+		$this->tranche = $tranche;
+		return $this;
+	}
+    public static function AfficherHeure(){
+
+        $req = MonPdo::getInstance()->prepare("select * from heure"); 
+        $req->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'heure');
+       
+        $req->execute();
+        $lesResultats = $req->fetchAll();
+        
+
+    
+
+        return $lesResultats;
+    }
+}
