e9b5e41671d9541431439e9ba16ad2e4bf8cfea3
lisa <lisa@MBP-de-lisa.lan>
2023-04-02 20:13:52 +0200

    class payer

---
+++ Model/trim.php
@@ -0,0 +1,55 @@
+<?php
+
+class Trim
+{
+
+    private $libelle;
+    private $dateFin;
+   
+
+
+	/**
+	 * @return mixed
+	 */
+	public function getLibelle() {
+		return $this->libelle;
+	}
+
+	/**
+	 * @param mixed $libelle 
+	 * @return self
+	 */
+	public function setLibelle($libelle): self {
+		$this->libelle = $libelle;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getDateFin() {
+		return $this->dateFin;
+	}
+
+	/**
+	 * @param mixed $dateFin 
+	 * @return self
+	 */
+	public function setDateFin($dateFin): self {
+		$this->dateFin = $dateFin;
+		return $this;
+	}
+    public static function AfficherTrim(){
+
+        $req = MonPdo::getInstance()->prepare("select * from trim ");
+        $req->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'prof');
+       
+        $req->execute();
+        $lesResultats = $req->fetchAll();
+        $nb_lignes = count($lesResultats);
+
+    
+
+        return $lesResultats;
+    }
+}
