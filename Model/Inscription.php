29372c650ab5877196ff1ded2df3e3d6aa2a60a0
lisa <lisa@MBP-de-lisa.lan>
2023-04-02 19:52:51 +0200

    class inscription

---
+++ Model/Inscription.php
@@ -0,0 +1,89 @@
+<?php
+
+class Inscription 
+{
+
+    private $idProf;
+    private $idEleve;
+    private $numSeance;
+    private $dateInscription;
+    
+
+
+	/**
+	 * @return mixed
+	 */
+	public function getIdProf() {
+		return $this->idProf;
+	}
+
+	/**
+	 * @param mixed $idProf 
+	 * @return self
+	 */
+	public function setIdProf($idProf): self {
+		$this->idProf = $idProf;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getIdEleve() {
+		return $this->idEleve;
+	}
+
+	/**
+	 * @param mixed $idEleve 
+	 * @return self
+	 */
+	public function setIdEleve($idEleve): self {
+		$this->idEleve = $idEleve;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getNumSeance() {
+		return $this->numSeance;
+	}
+
+	/**
+	 * @param mixed $numSeance 
+	 * @return self
+	 */
+	public function setNumSeance($numSeance): self {
+		$this->numSeance = $numSeance;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getDateInscription() {
+		return $this->dateInscription;
+	}
+
+	/**
+	 * @param mixed $dateInscription 
+	 * @return self
+	 */
+	public function setDateInscription($dateInscription): self {
+		$this->dateInscription = $dateInscription;
+		return $this;
+	}
+    public static function AfficherInscription(){
+
+        $req = MonPdo::getInstance()->prepare("select * from inscription "); //select IDPROF/IDELEVE from inscription 
+        $req->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'inscription');
+       
+        $req->execute();
+        $lesResultats = $req->fetchAll();
+        $nb_lignes = count($lesResultats);
+
+    
+
+        return $lesResultats;
+    }
+}
