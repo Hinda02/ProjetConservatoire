93aca9cd92920ba766ad60b039c488e84485e181
Hinda02 <hinda.h.habib@gmail.com>
2023-04-02 14:57:28 +0200

    tailwind config + navbar

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-    echo "hello bonjour";
+    include("View/header.php");
+    include("View/navbar.php");
 ?>
+<h1 class="text-3xl font-bold underline text-blue-600/100">
+    Hello world!
+  </h1>
