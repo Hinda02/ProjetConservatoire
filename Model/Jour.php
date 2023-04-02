481f0fb57d4622844223c648b910bd08af3bd653
lisa <lisa@MBP-de-lisa.lan>
2023-04-02 22:06:41 +0200

    ajout jour

---
+++ Model/Jour.php
@@ -0,0 +1,36 @@
+<?php
+
+class Jour {
+    
+    private $tranche;
+    
+
+	/**
+	 * @return mixed
+	 */
+	public function getTranche() {
+		return $this->tranche;
+	}
+
+	/**
+	 * @param mixed $tranche 
+	 * @return self
+	 */
+	public function setTranche($tranche): self {
+		$this->tranche = $tranche;
+		return $this;
+	}
+    public static function AfficherJour(){
+
+        $req = MonPdo::getInstance()->prepare("select * from jour ");
+        $req->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'jour');
+       
+        $req->execute();
+        $lesResultats = $req->fetchAll();
+        $nb_lignes = count($lesResultats);
+
+    
+
+        return $lesResultats;
+    }
+}
