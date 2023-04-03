fd3e7d25e07b746770de65c3aa6ecfb8d7ac1bb6
lisa <lisa@MacBook-Pro-de-lisa.local>
2023-04-03 11:22:49 +0200

    suppresion page connexion.php

--- Model/connexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-  //connexion à la base de données
-  $con = mysqli_connect("localhost","root","root","conserv");
-  if(!$con){
-     echo "Vous n'êtes pas connecté à la base de donnée";
-  }
-?>
