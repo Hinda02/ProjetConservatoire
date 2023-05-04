0d0b3f5a1f4d9c73e33afe12be37b1f6eaf824dc
Hinda02 <hinda.h.habib@gmail.com>
2023-05-04 09:44:45 +0200

    header change

--- View/header.php
+++ View/header.php
@@ -14,4 +14,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="Style.css">-->
 </head>
-<body class="">
+<body class="m-4">
