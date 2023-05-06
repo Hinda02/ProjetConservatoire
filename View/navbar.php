3966a3ea86b9b0c085d818778b62f35e339d906e
Hinda02 <hinda.h.habib@gmail.com>
2023-05-06 21:22:18 +0200

    sidebar fix

--- View/navbar.php
+++ View/navbar.php
@@ -65,10 +65,9 @@
 			
 		</div>
 		<a class="flex items-center justify-center w-full h-16 mt-auto bg-gray-200 hover:bg-gray-300" href="#">
-			<svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-			</svg>
-			<span class="ml-2 text-sm font-medium">Account</span>
+		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+			<path d="M16 17l5-5-5-5M19.8 12H9M10 3H4v18h6"/></svg>
+			<span class="ml-2 text-sm font-medium">Se déconnecter</span>
 		</a>
 	</div>
 	<!-- Component End  -->
