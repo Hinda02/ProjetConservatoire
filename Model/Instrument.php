369baa709894ce579d33cadeb3d429955cf5111f
lisaamm <mammoulisa.27@gmail.com>
2023-05-29 14:02:14 +0200

    ml

--- Model/Instrument.php
+++ Model/Instrument.php
@@ -26,14 +26,14 @@
         $req = MonPdo::getInstance()->prepare("select * from instrument 
                                                inner join prof on instrument.LIBELLE = prof.INSTRUMENT
                                                inner join seance on prof.IDPROF = seance.IDPROF
-                                               where lower(instrument.LIBELLE) like ('%:instrument%')");
+                                               where lower(instrument.LIBELLE) like (:instrument%)");
                                                
                                                      
         $req->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'seance');
         $req->bindParam('instrument', $instrument);
         $req->execute();
         $leResultat = $req->fetchAll();
-        
+        return $leResultat;
         }    
 
 
